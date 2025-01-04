5633d3e6b223eb309944f9559221dc7690135cb3
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2025-01-04 15:06:29 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,27 @@
 
+import React, { useState } from "react";
+
+const Selection = ({ applyColor }) => {
+  const [background, setBackground] = useState({ background: "white" });
+
+  const handleClick = () => {
+    applyColor(setBackground); // Call the parent's applyColor function
+  };
+
+  return (
+    <div
+      className="fix-box"
+      onClick={handleClick}
+      style={{
+        ...background,
+        width: "100px",
+        height: "100px",
+        margin: "10px",
+        border: "1px solid black",
+        cursor: "pointer",
+      }}
+    ></div>
+  );
+};
+
+export default Selection;
